9e253d60437259d71b78857eaae39832d74503fc
long ngo <long.ngoquang@vti.com.vn>
2022-11-14 00:31:28 +0700

    add code cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,4 +1,4 @@
-import { Col, Skeleton, Row, InputNumber } from "antd";
+import { Col, Skeleton, Row, InputNumber, Button } from "antd";
 import React, { useState, useEffect } from "react";
 import { Typography } from "antd";
 const { Title } = Typography;
@@ -7,6 +7,23 @@ const Cart = () => {
   const urlApi = "http://localhost:8765";
   const [carts, setCarts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const ORDER_STATUS_CONST = {
+    PREPARING: 0,
+    IN_PROGRESS: 1,
+    DONE: 2,
+  };
+
+  const CONFIG_ORDER_STATUS = {
+    [ORDER_STATUS_CONST.PREPARING]: {
+      message: "Đang chuẩn bị",
+      color: "primary",
+    },
+    [ORDER_STATUS_CONST.IN_PROGRESS]: {
+      message: "Đang trong kho",
+      color: "#5cdbd3",
+    },
+    [ORDER_STATUS_CONST.DONE]: { message: "Đã gửi", color: "#95de64" },
+  };
 
   useEffect(() => {
     const getCarts = async () => {
@@ -16,11 +33,7 @@ const Cart = () => {
         await fetch(`${urlApi}/carts/user/${userId}`)
           .then((response) => response.json())
           .then((response) => {
-            let products = [];
-            response.forEach((e) => {
-              products = [].concat(products, e.products);
-            });
-            setCarts(products);
+            setCarts(response);
           });
       }
       setLoading(false);
@@ -62,27 +75,46 @@ const Cart = () => {
               <Title level={4}>Total Items: {carts.length}</Title>
             </Col>
             {carts.map((cart) => {
+              const status = CONFIG_ORDER_STATUS[cart?.status || ORDER_STATUS_CONST.PREPARING]
               return (
-                <Col xs={12} lg={12} md={24}>
-                  <div style={{ display: "flex" }}>
-                    <img
-                      src={cart?.image}
-                      alt={cart?.title}
-                      height="100px"
-                      width="100px"
-                    />
-                    <div style={{ margin: "20px" }}>
-                      <h3 className="">{cart.title}</h3>
-                      <p>
-                        {"Số lượng: "}
-                        <InputNumber
-                          value={cart.quantity}
-                          disabled
-                        />
-                      </p>
-                      <h3 className="productCost">Price: {cart.price} $</h3>
-                    </div>
-                  </div>
+                <Col xs={24} style={{ border: "1px solid #ccc" }}>
+                  <Button type="primary" style={{ background: status.color, borderColor: status.color, marginTop: '10px', float: 'right' }}>{status.message}</Button>
+                  {cart.products.map((product, index) => {
+                    return (
+                      <Col xs={24}>
+                        <div
+                          style={{
+                            display: "flex",
+                            padding: "20px 0",
+                            paddingTop: index === 0 ? "0px" : "20px",
+                            width: "100%",
+                            borderBottom:
+                              index === cart.products.length - 1
+                                ? "none"
+                                : "1px solid #ccc",
+                          }}
+                        >
+                          <img
+                            src={product?.product?.image}
+                            alt={product?.product?.title}
+                            height="100px"
+                            width="100px"
+                          />
+                          <div style={{ margin: "20px" }}>
+                            <h3 className="">{product.product.title}</h3>
+                            <p>
+                              {"Quantity: "}
+                              <InputNumber value={product.quantity} disabled />
+                            </p>
+                            <h3 className="productCost">
+                              Price: {product.product.price * product.quantity}{" "}
+                              $
+                            </h3>
+                          </div>
+                        </div>
+                      </Col>
+                    );
+                  })}
                 </Col>
               );
             })}
